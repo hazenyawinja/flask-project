ff6a55fe440a1068c32fe9e9de55b6f9268ed28e
Hazeny <hazenyawinja@gmail.com>
2026-07-20 21:06:03 +0300

    data_tables

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from flask import Flask,render_template
+from database import get_products,get_sales,get_stock
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    x=5
+    name="hazeny"
+    numbers=[1,2,3,4,5,6,7,8,9,10]
+    for i in numbers:
+        if i%2==0:
+            print(i)
+    return render_template('index.html',y=x,a=name, numbers=numbers)
 
 @app.route("/products")
 def products():
-    return render_template('products.html')
+    products_data=get_products()
+    return render_template('products.html',products_data=products_data)
 
 @app.route("/stock")
 def stock():
-    return render_template('stock.html')
+    stock_data=get_stock()
+    return render_template('stock.html',stock_data=stock_data)
 
 @app.route("/sales")
 def sales():
-    return render_template('sales.html')
+    sales_data=get_sales()
+    return render_template('sales.html',sales_data=sales_data)
 
 @app.route("/dashboard")
 def dashboard():
@@ -30,4 +40,4 @@ def register():
 def login():
     return render_template('login.html')
 
-app.run()
+app.run(debug=True)
